bdcfa672ca60462799a1ddfcef93c4a102949b8b
KIMDAEWON <a62387272@gmail.com>
2025-01-07 16:26:07 +0900

    Test

---
+++ src/solve_10951.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class solve_10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+        String line;
+        while ((line = br.readLine()) != null) {
+            st = new StringTokenizer(line);
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            System.out.println(n+m);
+        }
+    }
+}
---
+++ src/solve_2475.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class solve_2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int a = Integer.parseInt(s[i]);
+            sum = (int) (sum + Math.pow(a, 2));
+        }
+        int tot = sum % 10;
+        System.out.println(tot);
+
+    }
+}
