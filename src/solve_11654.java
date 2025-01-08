6744d4cc837d2b18afb5a3c6990b01f17615d0f2
KIMDAEWON <a62387272@gmail.com>
2025-01-08 16:58:28 +0900

    1/8

---
+++ src/solve_11654.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class solve_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char a = br.readLine().charAt(0);
+        int asc = (int) a;
+        System.out.println(asc);
+    }
+}
