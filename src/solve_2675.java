1b3bb32363226b88bd7e381cea00450687e55ddc
KIMDAEWON <a62387272@gmail.com>
2025-01-12 00:55:57 +0900

    1/9

--- src/solve_2675.java
+++ src/solve_2675.java
@@ -1,10 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class solve_2675 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N =  Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < N; k++) {
+                    sb.append(s.charAt(j));
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 }
