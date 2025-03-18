82539378375ec6c1dc63c7d4e57b4167cc18d516
KIMDAEWON <a62387272@gmail.com>
2025-03-18 13:52:08 +0900

    3/18

--- src/swea/swea1952.java
+++ src/swea/swea1952.java
@@ -6,27 +6,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class swea1952 {
+
+    static int day, mon, mon3, year, ans = 0;
+    static int[] plan;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
         for (int T = 1; T <= t; T++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int day = Integer.parseInt(st.nextToken());
-            int mon = Integer.parseInt(st.nextToken());
-            int mon3 = Integer.parseInt(st.nextToken());
-            int year = Integer.parseInt(st.nextToken());
+            day = Integer.parseInt(st.nextToken());
+            mon = Integer.parseInt(st.nextToken());
+            mon3 = Integer.parseInt(st.nextToken());
+            year = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
-            int[] plan = new int[12];
+            plan = new int[12];
             for (int i = 0; i < 12; i++) {
                 plan[i] = Integer.parseInt(st.nextToken());
             }
 
+            ans = year;
+
+            dfs(0, 0);
+
+            System.out.println("#" + T + " " + ans);
         }
+
     }
 
-    static void dfs(){
+    static void dfs(int i, int sum){
+        if (i >= 12) {
+            ans = Math.min(sum, ans);
+            return;
+        }
 
+        dfs(i + 1, sum + day * plan[i]);
+
+        dfs(i + 1, sum + mon);
+
+        dfs(i + 3 , sum + mon3);
     }
 }
