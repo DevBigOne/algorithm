ec0dad396cc36df196ff5a2c8c10cb25803c7f4e
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-05-01 17:19:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 16792 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -17,17 +17,16 @@ public class Main {
         }
     }
 
-    static int n, k;
+    static int  n, k;
     static int min = Integer.MAX_VALUE;
     static int max = 100_000;
     static boolean[] visit;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+        
         visit = new boolean[100_001];
 
         if (n == k) {
@@ -36,13 +35,14 @@ public class Main {
             bfs();
             System.out.println(min);
         }
+
     }
 
-    static void bfs () {
+    static void bfs() {
         Queue<Node> q = new ArrayDeque<>();
         q.add(new Node(n, 0));
 
-        while (!q.isEmpty()) {
+        while (!q.isEmpty()){
             Node current = q.poll();
             // x: 현재 수빈이의 위치
             // time: 현재 시간
@@ -55,14 +55,14 @@ public class Main {
                 break;
             }
 
-            if (x - 1 >= 0 && !visit[x - 1]) {
-                visit[x - 1] = true;
-                q.add(new Node(x - 1, time + 1));
-            }
-
             if (x + 1 <= max && !visit[x + 1]) {
                 visit[x + 1] = true;
                 q.add(new Node(x + 1, time + 1));
+            }
+
+            if (x - 1 >= 0 && !visit[x - 1]) {
+                visit[x - 1] = true;
+                q.add(new Node(x - 1, time + 1));
             }
 
             if (x * 2 <= max && !visit[x * 2]) {
