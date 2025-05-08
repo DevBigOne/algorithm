b99f3ab55e545c5a1a69033b6b2c3728142b8ecc
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-05-08 12:24:51 +0900

    [Silver II] Title: N과 M (9), Time: 264 ms, Memory: 34256 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] seq, arr;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    static LinkedHashSet<String> ans;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n]; // 값 저장
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        seq = new int[m]; // 최종값저장
+        visit = new boolean[n]; // 방문해서 중복처리
+        ans = new LinkedHashSet<>();
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        for (String s : ans) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i : seq) {
+                sb.append(i).append(" ");
+            }
+            ans.add(sb.toString());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                seq[depth] = arr[i];
+                dfs(depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
