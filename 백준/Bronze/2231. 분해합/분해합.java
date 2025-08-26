b839d08c2fc345c3e1ca00f17150e446712d70cb
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-08-26 10:23:08 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = 0;
+        int s = 0;
+        for (int i = 0; i < N; i++) {
+            num = i;
+            int sum = 0;
+
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if(sum + i == N) {
+                s = i;
+                break;
+            }
+        }
+        System.out.println(s);
+    }
+}
