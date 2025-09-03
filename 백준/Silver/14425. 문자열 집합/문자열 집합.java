75f2a65c8e87e06eb529525e89667a9acaf4fa68
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-09-03 17:18:49 +0900

    [Silver IV] Title: 문자열 집합, Time: 2948 ms, Memory: 30792 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = br.readLine();
+        }
+
+        String[] arr = new String[m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = br.readLine();
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (s[i].equals(arr[j])) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
