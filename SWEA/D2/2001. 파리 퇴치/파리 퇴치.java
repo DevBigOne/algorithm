dbff5d7475429f3f7fba75d0f5aac8ecfd3e3d5c
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-09-05 14:21:12 +0900

    [D2] Title: 파리 퇴치, Time: 78 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -3,42 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-
+public class Solution{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        for (int t = 1; t <= T; t++) {
+        int T  = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t ++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int  M = Integer.parseInt(st.nextToken());
 
-            int[][] arr = new int[n][n];
-
-            int max = 0;
-            
-            for (int i = 0; i < arr.length; i++) {
+            int[][] arr = new int[N][N];
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < arr.length; j++) {
+                for (int j = 0; j < N; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            for (int i = 0; i <= n - m; i++) {  
-                for (int j = 0; j <= n - m; j++) {  
+
+            
+            int max = 0;
+            for (int i = 0; i <= N - M; i++) { // 전체 돌리기
+                for (int j = 0; j <= N - M; j++) {
                     int sum = 0;
-                    for (int k = i; k < i + m; k++) {
-                        for (int l = j; l < j + m; l++) {
+                    for (int k = i; k <  i + M; k++) { // m만큼 돌리기
+                        for (int l = j; l < j + M; l++) {
                             sum += arr[k][l];
                         }
                     }
-                    max = Math.max(max, sum);  
+                    max = Math.max(max, sum);
                 }
             }
-
-            System.out.println("#" + t + " " + max);
-
+            System.out.println("#"+ t + " " + max );
         }
-
     }
 }
