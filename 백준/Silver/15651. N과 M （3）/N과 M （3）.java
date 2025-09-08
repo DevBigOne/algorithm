a060fc97670822a5ce95a8e7f9a57c286e5e9ceb
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-09-08 16:33:14 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 67800 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static int n,m;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+
+        back( 0);
+        System.out.println(sb.toString());
+    }
+
+    private static void back( int cnt) {
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+                arr[cnt] = i;
+                back(cnt + 1);
+        }
+    }
+
+}
