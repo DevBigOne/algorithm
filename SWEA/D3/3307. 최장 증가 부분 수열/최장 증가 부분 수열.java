499257cb3de2b4ee05c460146c17d3b0392af3d6
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-09-10 16:39:16 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 137 ms, Memory: 34,216 KB -BaekjoonHub

---
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            int[] lis = new int[n];
+            int max = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int i = 0; i < n; i++) {
+                lis[i] = 1;
+                for (int j = 0; j < i ; j++) {
+                    if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+                        lis[i] = lis[j] + 1;
+                    }
+                }
+                max = Math.max(max, lis[i]);
+            }
+
+            System.out.println("#"+ t+" "+max);
+        }
+    }
+}
