c43c7435a28f9e52111bc37649589969040ed036
Kim_Dae_Won <133028211+DevBigOne@users.noreply.github.com>
2025-10-20 11:33:09 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.41 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 88 MB, 시간: 4.37 ms
+메모리: 84.7 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 20일 11:31:07
+2025년 10월 20일 11:33:07
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,47 +1,30 @@
-import java.util.*;
-import java.io.*;
 
 class Solution {
-    static int songjun;
-    static boolean[] visited;
-    public static List<Integer>[] friends;
-    
-    //dfs 메서드
-    public static void dfs(int start){
-        visited[start] = true;
-        songjun++;
-        for(int s : friends[start]){
-            if (!visited[s]){
-                dfs(s);
+    int N, min;
+    int[][] map;
+    int[] vst;
+    int dfs(int n){
+        vst[n] = 1;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(vst[i] == 0 && map[n][i] == 1) {
+                vst[i] = 1;
+                child += dfs(i);
             }
         }
-        return;
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;
     }
     public int solution(int n, int[][] wires) {
-        int answer = 1000;
-        for(int i = 0 ; i < wires.length; i++){
-            friends = new ArrayList[n+1]; //송전탑 관계 리스트 초기화
-            for(int k = 0 ; k < n+1; k++){
-                friends[k] = new ArrayList<Integer>();
-            }
-            //관계 설정하기
-            for(int j = 0 ; j < wires.length; j++){
-                if(j == i) continue;
-                friends[wires[j][0]].add(wires[j][1]);
-                friends[wires[j][1]].add(wires[j][0]);
-            }
-            //나뉜 전력망에서 송전탑의 개수 구해서 차 구하기
-            //(i에서 나온 wire 의 양 끝단을 dfs 시작점으로 구하자)
-            songjun = 0;
-            visited = new boolean[n+1];
-            dfs(wires[i][0]);
-            int result1 = songjun;
-            songjun = 0;
-            visited = new boolean[n+1];
-            dfs(wires[i][1]);
-            int result2 = songjun;
-            answer = Math.min(answer,Math.abs(result1-result2));
+        N = n;
+        min = n;
+        map = new int[n+1][n+1];
+        vst = new int[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
         }
-        return answer;
+        dfs(1);
+        return min;
     }
 }
